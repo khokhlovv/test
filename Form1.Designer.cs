fe7c9e94334de5813ea8d9602b46ed47043f9593
Yuriy <urikhokhlov3@yandex.ru>
2017-02-18 20:58:40 +0200

    generating labels

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,13 +30,14 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(77, 115);
+            this.button1.Location = new System.Drawing.Point(12, 210);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(137, 49);
+            this.button1.Size = new System.Drawing.Size(98, 40);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
@@ -51,15 +52,27 @@
             this.label1.TabIndex = 1;
             this.label1.Text = "label1";
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(174, 210);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(98, 40);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Changes for me";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -69,6 +82,7 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -17,9 +17,23 @@ namespace Test
             InitializeComponent();
         }
 
+        public int i = 0;
+
         private void button1_Click(object sender, EventArgs e)
         {
             label1.Text = "new text";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Random r = new Random();
+            Label l = new Label();
+            l.Text = "btn2 pressed";
+            i++;
+            l.Name = "lbl"+i;
+            l.Location = new Point(r.Next(Form1.ActiveForm.Width - 50), r.Next(Form1.ActiveForm.Height - 20));
+            Controls.Add(l);
+            this.Refresh();
         }
     }
 }
